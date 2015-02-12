98ecee46f06dda4f793a58e42f5355566d446707
thaddeus <thaddeus@sjccs142.saintjoe.edu>
2015-02-12 12:47:15 -0500

    Updated code 2/12/15 by thaddeus albert

--- BodyDriver.java
+++ BodyDriver.java
@@ -1,13 +1,27 @@
+/******************************************************************************
+	BodyDriver: a class to demonstrate and manipulate Body objects code from	 book typed in my Thaddeus Albert, Feb 12, 2015
+*******************************************************************************/ 
 public class BodyDriver {
 	public static void main(String[] args) {
 		Body sun = new Body();
-		sun.idNum = Body.nextID++;
+		// sun.idNum = Body.nextID++
 		sun.name = "Sol";
-		sun.orbits = null; // in solar system, sun is middle
-
-		Body earth = new Body();
-		earth.idNum = Body.nextID++;
-		earth.name = "Earth";
-		earth.orbits = sun; 
-	}
+		sun.orbits = null; // Sun dosen't orbit anything
+		System.out.println(sun.toString());
+		
+		Body earth = new Body("Earth", sun);
+		// earth.idNum = Body.nextID++;
+		earth.orbits = sun;
+		System.out.println(earth.toString());
+	}		
 }
+
+
+
+
+
+
+
+
+
+
