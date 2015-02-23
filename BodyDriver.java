9de77f814898772ec5a8ccee5a09eef1437e9f6e
thaddeus <thaddeus@sjccs142.saintjoe.edu>
2015-02-23 16:00:28 -0500

    re adding bodydriver to git hub for test purposes

--- BodyDriver.java
+++ BodyDriver.java
@@ -1,23 +1,24 @@
-/******************************************************************************
-	BodyDriver: a class to demonstrate and manipulate Body objects code from	 book typed in my Thaddeus Albert, Feb 12, 2015
-*******************************************************************************/ 
+/* Body Driver: a class to demonstrate and manipulate Body objects
+	code from book typed in by Brian Capouch
+	11 February 2015
+								*/
 public class BodyDriver {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 	// This uses the first constructor, no parameters
 	Body sun = new Body();
 	// sun.idNum = Body.nextID++;
 	sun.name = "Sol";
 	sun.orbits = null; // Sun don't orbit nobody
-	System.out.println(sun.toString());
+	System.out.println(sun.toString("Hi mom"));
 
 	// This uses the second constructor, two parameters
-	Body earth = new Body("Earth", sun);
+	Body earth = new Body(sun,"Earth");
 	// earth.idNum = Body.nextID++;
-	earth.orbits = sun;
+	// earth.orbits = sun;
 	System.out.println(earth.toString());
 	
-	// This uses the third constructor, one parameter
-	Body moon = new Body("Chuck", earth);
+	// This uses the third constructor, one parameterr
+	Body moon = new Body("Chuck");
 	moon.orbits = earth;
 	System.out.println(moon.toString());
 	
@@ -26,9 +27,40 @@ public class BodyDriver {
 	lance.name = "Lance";
 
 	System.out.println(lance.toString());
+	
 }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
